6c935da886e97b43b18def3acce72bdfba87262a
shijunhong <meair@qianyansoft.com>
2020-05-25 10:51:07 +0800

    paint style样式

--- app/src/main/java/com/shijunhong/fuckthecustomcontrols/one/BasisView.kt
+++ app/src/main/java/com/shijunhong/fuckthecustomcontrols/one/BasisView.kt
@@ -16,16 +16,33 @@ class BasisView : View {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+//        val paint = Paint()
+//        //设置画笔颜色
+//        paint.color = Color.RED
+//        //设置填充养殖
+//        paint.style = Paint.Style.STROKE
+//        //设置画笔宽度
+//        paint.strokeWidth = 50f
+//
+//        //画圆
+//        canvas.drawCircle(190f,200f,150f,paint)
+
+//        style   填充样式
+//        FILL 内部填充
+//        FILL_AND_STROKE  内部填充加描边
+//        STROKE  描边
+
+
         val paint = Paint()
-        //设置画笔颜色
         paint.color = Color.RED
-        //设置填充养殖
+        paint.strokeWidth = 10f
+        paint.style = Paint.Style.FILL
+        canvas.drawCircle(190f,120f,100f,paint)
+
+        paint.style = Paint.Style.FILL_AND_STROKE
+        canvas.drawCircle(190f,400f,100f,paint)
+
         paint.style = Paint.Style.STROKE
-        //设置画笔宽度
-        paint.strokeWidth = 50f
-
-        //画圆
-        canvas.drawCircle(190f,200f,150f,paint)
-
+        canvas.drawCircle(190f,700f,100f,paint)
     }
 }
